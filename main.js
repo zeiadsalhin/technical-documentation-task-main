35560862d76c60c8c2073317b9fb1295b40c2fda
zeiadsalhin <zeyadali011@gmail.com>
2023-04-21 20:20:03 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,63 @@
+function copy() {
+  const content = document.getElementById("code1").textContent;
+  navigator.clipboard.writeText(content);
+  alert("copied  !\n" + content);
+}
+// const content = document.getElementById("code1");
+// console.log(content);
+
+function copy2() {
+  const content = document.getElementById("code2").textContent;
+  navigator.clipboard.writeText(content);
+  alert("copied  !\n" + content);
+}
+//   const content = document.getElementById("code2");
+//   console.log(content);
+
+function copy3() {
+  const content = document.getElementById("code3").textContent;
+  navigator.clipboard.writeText(content);
+  alert("copied  !\n" + content);
+}
+//   const content = document.getElementById("code3");
+//   console.log(content);
+
+function copy4() {
+  const content = document.getElementById("code4").textContent;
+  navigator.clipboard.writeText(content);
+  alert("copied  !\n" + content);
+}
+const content = document.getElementById("code4");
+console.log(content);
+
+function copy5() {
+  const content = document.getElementById("code5").textContent;
+  navigator.clipboard.writeText(content);
+  alert("copied  !\n" + content);
+}
+//   const content = document.getElementById("code5");
+//   console.log(content);
+
+function copy6() {
+  const content = document.getElementById("code6").textContent;
+  navigator.clipboard.writeText(content);
+  alert("copied  !\n" + content);
+}
+//   const content = document.getElementById("code6");
+//   console.log(content);
+
+function copy7() {
+  const content = document.getElementById("code7").textContent;
+  navigator.clipboard.writeText(content);
+  alert("copied  !\n" + content);
+}
+//   const content = document.getElementById("code7");
+//   console.log(content);
+
+function copy8() {
+  const content = document.getElementById("code8").textContent;
+  navigator.clipboard.writeText(content);
+  alert("copied  !\n" + content);
+}
+//   const content = document.getElementById("code8");
+//   console.log(content);
